d59f30cb6a8c568566780bb02726e8767d809566
Sangeon Park <pse961202@gmail.com>
2017-05-24 12:29:24 +0900

    Add more aliases

--- functions.py
+++ functions.py
@@ -2,7 +2,8 @@ import datetime
 import random
 from market import yahoo, coinone, poloniex
 
-alias = {'빗코': 'BTC', '비트코인': 'BTC', '이더': 'ETH', '이클': 'ETC', '리플': 'XRP', 'zcash': 'ZEC'}
+alias = { '빗코': 'BTC', '비트코인': 'BTC', '이더': 'ETH', '이클': 'ETC', '리플': 'XRP', 'zcash': 'ZEC'
+        , '대시': 'DASH', '리스크': 'LSK', '스팀': 'STEEM', '모네로': 'XMR', '스텔라': 'STR'}
 
 def functionlist(msg):
     if '!편지' in msg:
