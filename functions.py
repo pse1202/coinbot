cafbbfe9f0f4768c88c7837a76de501d7150e763
Sangeon Park <pse961202@gmail.com>
2017-09-18 11:12:40 +0900

    Add currencies, aliases

--- functions.py
+++ functions.py
@@ -4,17 +4,11 @@ from market import yahoo, coinone, poloniex
 
 alias = { '비트': 'BTC', '빗코': 'BTC', '비트코인': 'BTC', '이더': 'ETH', '이클': 'ETC', 
         '리플': 'XRP', 'zcash': 'ZEC' , '대시': 'DASH', '리스크': 'LSK', '스팀': 'STEEM',
-        '모네로': 'XMR', '스텔라': 'STR', '*': 'all'}
+        '모네로': 'XMR', '스텔라': 'STR', '라코': 'LTC', '지캐시': 'ZEC', '*': 'all'}
 
 def functionlist(msg):
     if msg == 'PING':
         return 'PONG'
-
-    if msg.find('!환율') == 0:
-        currency = msg[4:].strip()
-        if len(currency) == 0:
-            currency = 'USD'
-        return str(yahoo.get_currency(currency))
 
     if msg.find('!코인') == 0:
         currency = msg[4:].strip()
